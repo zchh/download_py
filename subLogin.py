7e71e3e52e179ffb6d7745157e50f0571c7c147b
zchh <you@example.com>
2018-06-07 21:47:22 +0800

    commit all

--- subLogin.py
+++ subLogin.py
@@ -181,6 +181,7 @@ def main():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('headless')
     chrome_options.add_argument('no-sandbox')
+    chrome_options.add_argument("--disable-setuid-sandbox")
     browser = webdriver.Chrome(chrome_options=chrome_options) 
     browser.set_page_load_timeout(30)
     print('11')
